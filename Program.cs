337b1e2e382839ce752ee9f561b457a2a337d906
Geert Heremans <Esepidg$007>
2020-09-20 21:04:45 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Voorbeelden_van_operatoren
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int intUitkomst;
+
+            intUitkomst= 2 + 3 + 5;
+            Console.WriteLine(intUitkomst);//output is 10
+
+            intUitkomst = 5 - 2;
+            Console.WriteLine(intUitkomst);//output is 3
+
+            intUitkomst = 5 * 3;
+            Console.WriteLine(intUitkomst);//output is 15
+
+            intUitkomst = 10 / 2;
+            Console.WriteLine(intUitkomst);//output is 5
+
+
+            intUitkomst = 6 % 2;
+            Console.WriteLine(intUitkomst);//output is 0
+
+            intUitkomst = 6 % 4;
+            Console.WriteLine(intUitkomst);//output is 2 (rest na deling)
+
+            intUitkomst = 5;
+            intUitkomst++;
+            Console.WriteLine(intUitkomst);//output is 6
+
+            intUitkomst = 5;
+            intUitkomst--;
+            Console.WriteLine(intUitkomst);//output is 4
+
+        
+
+        }
+    }
+}
